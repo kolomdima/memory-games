47ba8d13726f4622cf7a2208fceb8e38346672e2
kolomdima <kolomdima111@gmail.com>
2022-08-30 18:24:28 +0200

    changes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var cards = document.querySelectorAll('.card');
 let cardName1 = '';
 let started = false;
 let startDate = new Date;
+let gameOver = false;
 
 
 [...cards].forEach((card)=>{
@@ -12,15 +13,18 @@ let startDate = new Date;
     }
     card.classList.toggle('is-flipped');
     if (cardName1 == '') {
-    cardName1 = card.getElementsByClassName('card__face--back')[0].id;
-    } else if (cardName1 == card.getElementsByClassName('card__face--back')[0].id) {
+    cardName1 = card.getElementsByClassName('card__face--back')[0].getAttribute('src');
+    } else if (cardName1 == card.getElementsByClassName('card__face--back')[0].getAttribute('src')) {
        [...document.getElementsByClassName('is-flipped')].forEach((element)=> {
         setTimeout(() => {
           element.classList.add('hidden');
           if (document.getElementsByClassName('hidden').length == document.getElementsByClassName('card').length) {
             let endDate = new Date;
-            let seconds = parseInt(((endDate - startDate) / 1000) % 60)
+            let seconds = parseInt(((endDate - startDate) / 1000) % 60);
+            if (!gameOver) {
+            gameOver = true;
             alert('You win! Result is ' + seconds + ' seconds');
+            }
          }
         }, 1000);   
        });
